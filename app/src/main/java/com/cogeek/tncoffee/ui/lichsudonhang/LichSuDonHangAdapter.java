56967535a01754386fc0009824230f6ea90cac34
kuroitsuki2303 <82944085+kuroitsuki2303@users.noreply.github.com>
2021-05-02 22:06:00 +0700

    lichsudonhangFragmentAdapter

---
+++ app/src/main/java/com/cogeek/tncoffee/ui/lichsudonhang/LichSuDonHangAdapter.java
@@ -0,0 +1,56 @@
+package com.cogeek.tncoffee.ui.lichsudonhang;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.cogeek.tncoffee.R;
+import com.cogeek.tncoffee.models.LichSuDonHang;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class LichSuDonHangAdapter extends ArrayAdapter<LichSuDonHang> {
+    Activity context;       // màn hình sử dụng layout này (giao diện này)
+    int resource;           // Layout cho từng dòng muốn hiển thị (làm theo khách hàng)
+    List<LichSuDonHang> objects;   // danh sách nguồn dữ liệu muốn hiển thị lên giao diện
+    // chính là item (không lấy item vì rất khó kế thừa)
+    public LichSuDonHangAdapter(@NonNull Activity context, int resource, @NonNull List<LichSuDonHang> objects) {
+        super(context, resource, objects);
+        this.context = context; //this là tham chiếu hiện tại
+        this.resource = resource;
+        this.objects = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.item_lichsudonhang, null, true);
+        }
+        LichSuDonHang lichSuDonHang = getItem(position);
+
+
+        ImageView imgHinh =(ImageView) convertView.findViewById(R.id.imgHinhDH);
+        Picasso.get().load(lichSuDonHang.getHinh()).into(imgHinh);
+
+        TextView txtTen = convertView.findViewById(R.id.txtTenDonHang);
+        txtTen.setText(lichSuDonHang.getTen());
+
+        TextView txtSoSanPham = convertView.findViewById(R.id.txtSoSanPham);
+        txtSoSanPham.setText(String.valueOf(lichSuDonHang.getSoSanPham()));
+
+        TextView txtSoTien = convertView.findViewById(R.id.txtSoTienDonHang);
+        txtSoTien.setText(String.valueOf(lichSuDonHang.getSoTien()));
+        
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/cogeek/tncoffee/ui/lichsudonhang/LichSuDonHangFragment.java
@@ -0,0 +1,37 @@
+package com.cogeek.tncoffee.ui.lichsudonhang;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cogeek.tncoffee.models.LichSuDonHang;
+import com.cogeek.tncoffee.R;
+
+import java.util.ArrayList;
+
+public class LichSuDonHangFragment extends AppCompatActivity {
+    ListView lvLichSuDonHang;
+    ArrayList<LichSuDonHang> dsLichSuDonHang;
+    LichSuDonHangAdapter lichSuDonHangAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lich_su_don_hang);
+
+        addControls();
+    }
+
+
+    private void addControls() {
+        lvLichSuDonHang = findViewById(R.id.lvLichSuDonHang);
+        dsLichSuDonHang = new ArrayList<>();
+        dsLichSuDonHang.add(new LichSuDonHang("https://cafebiz.cafebizcdn.vn/2019/7/23/photo-1-15638793092381493027519.jpg","Truong Ngoc Anh Thu", 1,23000));
+        //dsLichSuDonHang.add(new com.example.models.LichSuDonHang());
+
+        lichSuDonHangAdapter = new LichSuDonHangAdapter(LichSuDonHangFragment.this, R.layout.item_lichsudonhang, dsLichSuDonHang);
+
+        lvLichSuDonHang.setAdapter(lichSuDonHangAdapter);
+    }
+}
