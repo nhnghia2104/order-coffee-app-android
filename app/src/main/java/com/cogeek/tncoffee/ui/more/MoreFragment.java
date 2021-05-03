91c7785854d104786ce7d1e635bc0a46d0ce9d25
kuroitsuki2303 <82944085+kuroitsuki2303@users.noreply.github.com>
2021-05-03 20:58:18 +0700

    click on button and open user info fragment

--- app/src/main/java/com/cogeek/tncoffee/ui/more/MoreFragment.java
+++ app/src/main/java/com/cogeek/tncoffee/ui/more/MoreFragment.java
@@ -5,25 +5,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.cogeek.tncoffee.LaunchActivity;
 import com.cogeek.tncoffee.R;
 import com.cogeek.tncoffee.models.Store;
 import com.cogeek.tncoffee.ui.store.StoreAdapter;
+import com.cogeek.tncoffee.ui.userinfo.UserInfoFragment;
 import com.cogeek.tncoffee.utils.SharedHelper;
+import com.google.firebase.auth.UserInfo;
 
 import java.util.ArrayList;
+
+import kotlin.collections.IntIterator;
+
+import static com.cogeek.tncoffee.R.id.vInfo;
 
 public class MoreFragment extends Fragment {
 
     private ListView listView;
     private StoreAdapter storeAdapter;
     private ArrayList<Store> arrayList;
+    private Button btnUserInfo;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +45,7 @@ public class MoreFragment extends Fragment {
         return root;
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -42,7 +53,25 @@ public class MoreFragment extends Fragment {
             logout();
         });
 
+        btnUserInfo = view.findViewById(vInfo);
+        btnUserInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openUserInfo();
+            }
+        });
     }
+
+    private void openUserInfo() {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setReorderingAllowed(true);
+        transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.fade_out,R.anim.slide_in_left, R.anim.fade_out);
+        transaction.addToBackStack(null);
+        transaction.add(R.id.vInfo, UserInfoFragment.class, null).commit();
+    }
+
+    //jump to user info layout
     private void logout() {
         SharedHelper.getInstance(getActivity()).logout();
         Intent intent = new Intent(getActivity(), LaunchActivity.class);
