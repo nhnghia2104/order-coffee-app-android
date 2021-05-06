d358e2b2472172979543e13142d977de6912f965
kuroitsuki2303 <82944085+kuroitsuki2303@users.noreply.github.com>
2021-05-06 17:25:58 +0700

    rar

--- app/src/main/java/com/cogeek/tncoffee/ui/userinfo/UserInfoFragment.java
+++ app/src/main/java/com/cogeek/tncoffee/ui/userinfo/UserInfoFragment.java
@@ -1,13 +1,17 @@
 package com.cogeek.tncoffee.ui.userinfo;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -32,7 +36,7 @@ public class UserInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_info, container, false);
+        return inflater.inflate(R.layout.activity_edit_info, container, false);
     }
 
     @Override
